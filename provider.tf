6d20c5934ad9c60ecb3632dddecd0a965d0a7449
Sridhar Peteti <p.sridhar96@gmail.com>
2023-01-09 19:18:31 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_files = ["$HOME/.aws/credentials"]
-  shared_config_files =["$HOME/.aws/config"]
+  shared_credentials_files = "$HOME/.aws/credentials"
+  shared_config_files ="$HOME/.aws/config"
   profile = "default"
 }
