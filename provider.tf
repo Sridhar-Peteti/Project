36a1001384777669d0c370ccb1112a4bdd80c5af
Sridhar Peteti <p.sridhar96@gmail.com>
2023-01-09 18:24:33 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
-  shared_credentials_files = ["$HOME/.aws/credentials"]
+  shared_config_files = "$HOME/.aws/config"
+  shared_credentials_files = "$HOME/.aws/credentials"
   profile = "default"
 }
