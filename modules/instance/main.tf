58d60233aa3b3e057a4d7fa895c2b8b923905535
Sridhar Peteti <p.sridhar96@gmail.com>
2023-01-10 16:08:37 +0100

    Update main.tf

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -1,8 +1,41 @@
-resource "aws_instance" "keyinst" {
-ami = var.ami_id
-instance_type = var.inst_type
-for_each = var.inst_names
-tags = {
-Name = each.value
-}
+  resource "aws_instance" "keyinst" {
+  ami = var.ami_id
+  instance_type = var.inst_type
+  security_groups = ["aws_security_group.TF-SG.name"]
+  for_each = var.inst_names
+  tags = {
+  Name = each.value
+  }
+  }
+
+  resource "aws_security_group" "TF-SG" {
+  name        = "TF-SG"
+  description = "Adding terraform security group"
+  vpc_id      = "vpc-044b7285d31591717"
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "TF-SG"
+  }
 }
