c12cb1f96a638ccf52b5748a1247c1522c1e15aa
Sridhar Peteti <p.sridhar96@gmail.com>
2023-01-10 15:28:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-module "my-instance" {
-source = "./modules/instance/"
-}
-
-module "my-vpc" {
+/*module "my-vpc" {
 source = "./modules/vpc/"
 }
 
 module "my-s3" {
 source = "./modules/s3/"
+}*/
+
+module "my-instance" {
+source = "./modules/instance/"
 }
